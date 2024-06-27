56cc44d197eb223fcd4b895e06a52e974ecb842d
DonavanGrobler <donavangrobler2@gmail.com>
2024-06-27 20:44:34 +0200

    Adds new user routes for logging in, signing up and validates email

---
+++ controllers/users-controllers.js
@@ -0,0 +1,55 @@
+const uuid = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "u1",
+    name: "Donavan Grobler",
+    email: "test@gmail.com",
+    password: "1forrest1",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_PLACES });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_PLACES.find((u) => u.email === email);
+
+  if (hasUser) {
+    throw new HttpError("Could not create user, email already exists", 422);
+  }
+
+  const createUser = {
+    id: uuid.v4(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_PLACES.push(createUser);
+
+  res.status(201).json({ user: createUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_PLACES.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError(
+      "Could not identify user, credentials might be wrong",
+      401
+    );
+  }
+
+  res.json({ message: "Logged in!" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
