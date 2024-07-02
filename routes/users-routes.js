2a5dee1a39373b08025a36cd3aedab7792d6eef3
DonavanGrobler <donavangrobler2@gmail.com>
2024-07-02 20:34:37 +0200

    Adds and uses multer for file uploads

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const usersControllers = require("../controllers/users-controllers");
 
+const fileUpload = require("../middleware/file-upload");
+
 const router = express.Router();
 
 const { check } = require("express-validator");
@@ -10,6 +12,7 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
