b8396e005fa3a4f45d7b4b7b572db52f5cccf2fb
DonavanGrobler <donavangrobler2@gmail.com>
2024-07-03 22:43:36 +0200

    Adds frontend to the back end for deploying

--- app.js
+++ app.js
@@ -12,24 +12,14 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
+app.use(express.static(path.join("public")));
 
 app.use("/api/places", placesRoutes);
 
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find route", 400);
-  throw error;
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
 app.use((error, req, res, next) => {
